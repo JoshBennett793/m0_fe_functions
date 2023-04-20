9bdf8e66584a287ae79f6e2043d6ee914ef72538
Josh Bennett <joshbennett793@yahoo.com>
2023-04-20 13:54:51 -0500

    Write and document methods for section 2

--- builtInMethods.js
+++ builtInMethods.js
@@ -52,6 +52,28 @@ Since "Hello World" does end with the string "rld" the return value is true.
 var firstName = "Jeff";
 console.log(firstName.startsWith("J"));
 
+/* 
+The charAt() method is called on the lastName variable, which stores the string object "Spicoli".
+The charAt() method will return a new string with the character located at the index of the string
+that pertains to the number passed in as an argument.
+In this example, the return value is "S" because the character located at index 0 of "Spicoli" is "S".
+The console.log() statement prints the return value of the charAt() method performed on the 
+string "Spicoli", logging "S" to the console.
+*/
+var lastName = "Spicoli";
+console.log(lastName.charAt(0));
+
+/* 
+The slice() method is called on the movie variable, which stores the string object "Fast Times at Ridgemont High".
+The slice() method will return a new string with the characters found inbetween the index positions indicated by the 1st and 2nd number passed in as arguments.
+In this example, slice() will start at index 0 and end at the character before index 11, making
+the return value "Fast Times" because the "F" is at index 0 and the "S" in "Times" is the character
+just before index 11. 
+The console.log() statement prints the return value of the slice() method called on the movie string,
+logging "Fast Times" to the console.
+*/
+var movie = "Fast Times at Ridgemont High";
+console.log(movie.slice(0, 11));
 
 
 // SECTION 3: Calling methods on variables assigned to arrays.
